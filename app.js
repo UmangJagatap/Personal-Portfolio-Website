4594bd5093cdf25ae10f5e532f565e454c2c9568
UmangJagatap <122389993+UmangJagatap@users.noreply.github.com>
2023-11-10 21:16:59 +0530

    light mode complete

--- app.js
+++ app.js
@@ -46,6 +46,13 @@ function pageTransitions() {
             element.classList.add('active');
         }
     })
+
+    //Toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',() => {
+        let element = document.body;
+        element.classList.toggle('light-mode');
+    })
 }
 
 pageTransitions();
